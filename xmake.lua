44e5e9a9f12ce966ce25705c056de443cdf0a2d8
ws3917 <ws3917@outlook.com>
2026-01-01 14:53:18 +0800

    init

---
+++ xmake.lua
@@ -0,0 +1,52 @@
+add_requires(
+    "cpr",
+    "glfw",
+    "nlohmann_json",
+    "picosha2"
+)
+add_rules("mode.debug", "mode.release")
+set_languages("c++20")
+
+for _, libname in ipairs(vendored_libs) do
+    add_requires(libname, {system = true})
+end
+target("deltarune_cnpatcher")
+    set_kind("binary")
+
+    -- source files
+    add_files("src/main.cpp")
+    add_files("src/**/*.cpp")
+
+    -- packages
+    add_packages(
+        "cpr",
+        "glfw",
+        "nlohmann_json",
+        "picosha2"
+    )
+    add_packages(table.unpack(vendored_libs))
+    -- defines
+    add_defines("UNICODE", "_UNICODE")
+
+    before_build(function (target)
+        -- clean target output directory before each build
+        local outdir = target:targetdir()
+        if outdir and os.isdir(outdir) then
+            os.rmdir(outdir)
+        end
+    end)
+
+    if is_plat("windows") and is_mode("release") then
+        add_files("app/win.rc")
+        add_ldflags("/SUBSYSTEM:WINDOWS");
+    end
+
+    after_build(function (target)
+        local outdir = target:targetdir()
+        os.cp(path.join(os.projectdir(), "external"), outdir)
+        os.cp(path.join(os.projectdir(), "image"), outdir)
+        os.cp(path.join(os.projectdir(), "lang"), outdir)
+        os.cp(path.join(os.projectdir(), "font"), outdir)
+        os.cp(path.join(os.projectdir(), "readme*.txt"), outdir)
+        os.cp(path.join(os.projectdir(), "dlls/*.dll"), outdir)
+    end)
