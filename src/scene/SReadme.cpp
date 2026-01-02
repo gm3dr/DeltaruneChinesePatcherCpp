9b904ef8cc12f24200e0e890a154d2173bc1a13f
ws3917 <ws3917@outlook.com>
2026-01-02 13:20:44 +0800

    1

--- src/scene/SReadme.cpp
+++ src/scene/SReadme.cpp
@@ -1,23 +1,7 @@
 ﻿#include "SReadme.h"
 #include <SDL3_image/SDL_image.h>
-#include <iostream>
-void SReadme::Enter() {
-  animBgAnim.SetAtlas(&Asset::imgBgAnim);
-  animBgAnim.SetInterval(300);
-  animBgAnim.SetLoop(true);
-}
-void SReadme::Update(int delta) {
-  camera.Update(delta);
-  animBgAnim.Update(delta);
-}
-void SReadme::Input(SDL_Event *event) {
-  if (event->type == SDL_EVENT_KEY_DOWN &&
-      event->key.scancode == SDL_SCANCODE_A) {
-    camera.Shake();
-  }
-}
-void SReadme::Draw(SDL_Renderer *renderer) {
-  Vector2 cameraPos = camera.GetPosition();
-  animBgAnim.Draw(renderer, 200 - cameraPos.x, 200 - cameraPos.y);
-}
-void SReadme::Exit() { std::cout << "Readme离开场景" << std::endl; }
+void SReadme::Enter() {}
+void SReadme::Update(int delta) {}
+void SReadme::Input(SDL_Event *event) {}
+void SReadme::Draw(SDL_Renderer *renderer) {}
+void SReadme::Exit() {}
