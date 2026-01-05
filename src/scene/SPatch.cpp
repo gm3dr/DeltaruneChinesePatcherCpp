a196240b776a06ebf79cd60da02011d5edb3030d
ws3917 <ws3917@outlook.com>
2026-01-05 12:52:17 +0800

    add code

---
+++ src/scene/SPatch.cpp
@@ -0,0 +1,6 @@
+﻿#include "SPatch.h"
+void SPatch::Enter() {}
+void SPatch::Update(int delta) {}
+void SPatch::Input(SDL_Event *event) {}
+void SPatch::Draw(SDL_Renderer *renderer, Camera &camera) {}
+void SPatch::Exit() {}
